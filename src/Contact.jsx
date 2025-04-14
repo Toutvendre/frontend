203af543e3d3072605f2c05e994b21d2e06b3562
Toutvendre <ducvfx07@gmail.com>
2025-04-14 14:20:06 +0000

    Add contacts

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -24,7 +24,7 @@ function Contact() {
         setIsLoading(true);
 
         try {
-            const response = await axios.post(`${import.meta.env.VITE_API_URL}`, formData, {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/contact`, formData, {
                 headers: { 'Content-Type': 'application/json' },
             });
             setFormStatus(response.data);
